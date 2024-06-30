a69d0afa09f94507948de5e21404487068417a59
JHKru <jhkr@web.de>
2024-06-30 23:48:43 +0200

    Modified environment.yml

--- src/biotraj/version.py
+++ src/biotraj/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev2+gadb7a75.d20240630'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'gadb7a75.d20240630')
+__version__ = version = '0.1.dev3+g4a964ff.d20240630'
+__version_tuple__ = version_tuple = (0, 1, 'dev3', 'g4a964ff.d20240630')
